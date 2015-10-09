2353ac554f2a791473538ad426df4880af1dd50a
Davtux <barrat.romain87@gmail.com>
2015-10-09 19:06:53 +0200

    On avance mais rien ne marche

--- Compilateur/main.c
+++ Compilateur/main.c
@@ -1,61 +1,80 @@
 //Inclusions des bibliothèques :
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <string.h>
+#include <tables.c>
 //Inclusion des fichiers annexes :
 
 
 //Déclaration des variables globales :
+//#include "tables.c"
 
-//Mise en place de la table lexicale :
+//table des correspondances
+char tableCorrespondance[36];
+
+//table lexicale
 char tableLexicale[26][26];
 //TODO
-
-
 
 
 int main(int argc, char *argv[])
 {
     //Déclaration des variables :
 
-    char *chaineActuelle[];
+    char *chaineActuelle;
     char caractereActuel;
     char caractereSuivant;
-    // char tableLexicale[26][26];
-    char *tableauRenduFinal[];
-
+    char *tableauRenduFinal;
     int motReconnu = 0;
-
+    int indexCaracActuel;
 
     //Vérification des arguments :
 
     if (argc < 2){
-        printf("Il est nécessaire de fournir au moins un argument à ce programme.");
+        printf("Il est nécessaire de fournir au moins un argument à ce programme.\n");
         exit(1);
     }
 
+    //Initialisation des tables
+    initialiserTableCorrespondance(*tableLexicale);
+
+    //Initialisation du tableau de rendu
+    tableauRenduFinal = "Ce n'est pas normal que ceci soit affiché";
+
     //Lecture des arguments
-    for(int i = 1; i < argc; i++){
+    for( int i = 1; i < argc; i++){
         chaineActuelle = argv[i];
 
         //Décomposition de l'argument caractère par caractère
-        for(int j = 0; chaineActuelle[j] != '/0'; j++){
+        for( int j = 0; strcmp(chaineActuelle[j], '/0') == 0; j++){
             caractereActuel = chaineActuelle[j];
 
+            //On cherche à quelle ligne se trouve le caractère dans la table lexicale :
+            for (indexCaracActuel = 0; caractereActuel == tableCorrespondance[indexCaracActuel]; indexCaracActuel++) //Cas de caractère non présent dans la table non géré !
+
             //recherche des caractères suivants possibles dans la table lexicale
-            for(int ligne = 0; ligne < 26; ligne ++){
                 for(int colonne = 0; colonne < 26; colonne ++){
+                        if(tableLexicale[indexCaracActuel][colonne] == 1)
+
                         printf("TODO");
                         exit(0);
                     }
                 }
             }
+        //on vérifie si le mot a été reconnu
         if(motReconnu != 0)
-            tableauRenduFinal += chaineActuelle + '\n'; // +'\n' pour effectuer un retour à la ligne automatique
+            //mot reconnu :
+            // TODO : trouver un moyen de savoir de quel type est el mot : nombre, chaine de carac… (un case ?)
+            tableauRenduFinal = strcat(tableauRenduFinal, "Type : ");
+        else
+            //mot inconnu
+            tableauRenduFinal = strcat(tableauRenduFinal, "Inconnu : ");
 
-    }
-    printf("Voici ler mots reconnus par le programme : \n \n");
-    printf("%c",tableauRenduFinal[j]);
+        //on ajoute le mot à l'ffichage final
+        tableauRenduFinal = strcat(tableauRenduFinal, chaineActuelle);
+        tableauRenduFinal = strcat(tableauRenduFinal, '\n'); //On rajoute '\n' afin de ne pas avoir à gérer les retours à la ligne
 
+    printf('Voici les mots reconnus par le programme : \n \n');
+    //printf("%c", &tableau);
     exit(0);
 }
-
---
+++ Compilateur/tables.c
@@ -0,0 +1,45 @@
+//table des correspondances
+
+void initialiserTableCorrespondance(char tableCorrespondance[]){
+    tableCorrespondance[0] = "a";
+    tableCorrespondance[1] = 'b';
+    tableCorrespondance[2] = 'c';
+    tableCorrespondance[3] = 'd';
+    tableCorrespondance[4] = 'e';
+    tableCorrespondance[5] = 'j';
+    tableCorrespondance[6] = 'g';
+    tableCorrespondance[7] = 'h';
+    tableCorrespondance[8] = 'i';
+    tableCorrespondance[9] = 'j';
+    tableCorrespondance[10] = 'k';
+    tableCorrespondance[11] = 'l';
+    tableCorrespondance[12] = 'm';
+    tableCorrespondance[13] = 'n';
+    tableCorrespondance[14] = 'o';
+    tableCorrespondance[15] = 'p';
+    tableCorrespondance[16] = 'q';
+    tableCorrespondance[17] = 'r';
+    tableCorrespondance[18] = 's';
+    tableCorrespondance[19] = 't';
+    tableCorrespondance[20] = 'u';
+    tableCorrespondance[21] = 'v';
+    tableCorrespondance[22] = 'w';
+    tableCorrespondance[23] = 'x';
+    tableCorrespondance[24] = 'y';
+    tableCorrespondance[25] = 'z';
+    tableCorrespondance[26] = '0';
+    tableCorrespondance[27] = '1';
+    tableCorrespondance[28] = '2';
+    tableCorrespondance[29] = '3';
+    tableCorrespondance[30] = '4';
+    tableCorrespondance[31] = '5';
+    tableCorrespondance[32] = '6';
+    tableCorrespondance[33] = '7';
+    tableCorrespondance[34] = '8';
+    tableCorrespondance[35] = '9';
+}
+
+//table lexicale
+
+void initialiserTableLexicale(char table[]){
+//TODO
